14defe82a998c9c85d8ba31e6bc8f92b1971086f
egay <egfojas@voyagerinnovation.com>
2014-08-14 10:33:18 +0800

    Created empty xCode project

---
+++ EAExpandableTableView/EAExpandableTableView/EAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EAAppDelegate.h
+//  EAExpandableTableView
+//
+//  Created by Edgar Allan Fojas on 8/14/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EAExpandableTableView/EAExpandableTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EAExpandableTableView
+//
+//  Created by Edgar Allan Fojas on 8/14/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EAAppDelegate class]));
+    }
+}
