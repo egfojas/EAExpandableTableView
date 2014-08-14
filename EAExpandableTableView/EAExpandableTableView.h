427f9df54e4952b72a1f0a8db56c734010249fbf
egay <egfojas@voyagerinnovation.com>
2014-08-14 10:34:39 +0800

    Created EAExpandableTableView class

---
+++ EAExpandableTableView/EAExpandableTableView.h
@@ -0,0 +1,13 @@
+//
+//  EAExpandableTableView.h
+//  EAExpandableTableView
+//
+//  Created by Edgar Allan Fojas on 8/14/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAExpandableTableView : UITableView
+
+@end
---
+++ EAExpandableTableView/EAExpandableTableView.m
@@ -0,0 +1,31 @@
+//
+//  EAExpandableTableView.m
+//  EAExpandableTableView
+//
+//  Created by Edgar Allan Fojas on 8/14/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import "EAExpandableTableView.h"
+
+@implementation EAExpandableTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
